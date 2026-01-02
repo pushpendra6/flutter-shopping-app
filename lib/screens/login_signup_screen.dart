16ac49606a8d570c84c57321eb46f4a5ef3a4295
Pushpendra Singh Shekhawat <shekhawatpss9783@gmail.com>
2026-01-02 17:20:08 +0530

    login screen ui updated

--- lib/screens/login_signup_screen.dart
+++ lib/screens/login_signup_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class LoginSignupScreen extends StatelessWidget {
+class LoginSignupScreen extends StatefulWidget {
   const LoginSignupScreen({super.key});
+
+  @override
+  State<LoginSignupScreen> createState() => _LoginSignupScreenState();
+}
+
+class _LoginSignupScreenState extends State<LoginSignupScreen> {
+  bool isLoading = false;
+  final TextEditingController otpController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -9,45 +17,70 @@ class LoginSignupScreen extends StatelessWidget {
       backgroundColor: const Color(0xFFF8FAFD),
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(16),
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-                children: [
-                  Text(
-                    'Welcome To',
-                    style: TextStyle(
-                      color: Colors.black87, // Shoe color
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
+              Text(
+                'Welcome To',
+                style: TextStyle(
+                  color: Colors.black87, // Shoe color
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Shoe',
+                      style: TextStyle(
+                        color: Colors.black87, // Shoe color
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: 'Shoe',
-                          style: TextStyle(
-                            color: Colors.black87, // Shoe color
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        TextSpan(
-                          text: 'World',
-                          style: TextStyle(
-                            color: Theme.of(
-                              context,
-                            ).colorScheme.primary, // World color
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
+                    TextSpan(
+                      text: 'World',
+                      style: TextStyle(
+                        color: Theme.of(
+                          context,
+                        ).colorScheme.primary, // World color
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                keyboardType: TextInputType.phone,
+                maxLength: 10,
+                decoration: InputDecoration(
+                  hintText: 'Enter Your Mobile Number',
+                  prefixIcon: Icon(Icons.phone),
+                  counterText: '',
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  FocusScope.of(context).unfocus();
+                },
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Theme.of(context).colorScheme.primary),
+                  elevation: WidgetStatePropertyAll(10),
+                ),
+              
+                
+                child: Text(
+                  'Send OTP',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
                   ),
-                ],
+                ),
               ),
             ],
           ),
